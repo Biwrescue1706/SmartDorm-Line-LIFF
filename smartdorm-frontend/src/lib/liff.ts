876f80bfb90942eea59b9c17c12eee306bc0410f
Biwbong <bewrockgame1@gmail.com>
2025-10-01 13:43:03 +0700

    13.43

--- smartdorm-frontend/src/lib/liff.ts
+++ smartdorm-frontend/src/lib/liff.ts
@@ -1,9 +1,7 @@
 // src/lib/liff.ts
 import liff from "@line/liff";
 
-/**
- * 🔹 เริ่มต้น LIFF
- */
+//🔹 เริ่มต้น LIFF
 export async function initLIFF() {
   try {
     await liff.init({ liffId: "2008099518-23vqwKZY" });
@@ -26,23 +24,4 @@ export async function initLIFF() {
   } catch (err) {
     console.error("❌ LIFF init error:", err);
   }
-}
-
-/**
- * 🔹 ดึงข้อมูล user ที่ login ผ่าน LIFF
- */
-export function getLiffUser() {
-  const userId = localStorage.getItem("liff_userId");
-  const displayName = localStorage.getItem("liff_displayName");
-  return { userId, displayName };
-}
-
-/**
- * 🔹 Logout ออกจาก LIFF + ลบข้อมูลใน localStorage
- */
-export function logoutLIFF() {
-  liff.logout();
-  localStorage.removeItem("liff_userId");
-  localStorage.removeItem("liff_displayName");
-  window.location.reload();
 }
--- smartdorm-frontend/src/pages/UploadSlip.tsx
+++ smartdorm-frontend/src/pages/UploadSlip.tsx
@@ -1,6 +1,8 @@
 import { useLocation, useNavigate } from "react-router-dom";
 import UploadSlipForm from "../components/UploadSlip/UploadSlipForm"; // ✅ ใช้ path ที่ถูกต้อง
 import type { Room } from "../types/Room";
+import { useEffect } from "react";
+import Swal from "sweetalert2";
 
 export default function UploadSlip() {
   const { state } = useLocation();
@@ -8,14 +10,14 @@ export default function UploadSlip() {
   const room = state as Room;
 
   // ✅ ตรวจสอบ login ก่อน
-  // useEffect(() => {
-  //   const userId = localStorage.getItem("liff_userId");
-  //   if (!userId) {
-  //     Swal.fire("⚠️ กรุณาเข้าสู่ระบบผ่าน LINE", "", "warning").then(() => {
-  //       nav("/");
-  //     });
-  //   }
-  // }, [nav]);
+  useEffect(() => {
+    const userId = localStorage.getItem("liff_userId");
+    if (!userId) {
+      Swal.fire("⚠️ กรุณาเข้าสู่ระบบผ่าน LINE", "", "warning").then(() => {
+        nav("/");
+      });
+    }
+  }, [nav]);
 
   return (
     <div className="uploadslip-container py-4">
