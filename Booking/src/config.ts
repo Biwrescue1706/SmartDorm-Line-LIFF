c5cd3d220bd85821fad8af6d8f0624aca5d01dd3
Biwbong <bewrockgame1@gmail.com>
2025-10-23 13:32:51 +0700

    Update config.ts

--- Booking/src/config.ts
+++ Booking/src/config.ts
@@ -1,1 +1,2 @@
-export const API_BASE = "https://smartdorm-backend.onrender.com";
+export const API_BASE = "https://smartdorm-backend.biwbong.shop";
+//export const API_BASE = "http://localhost:10000";
