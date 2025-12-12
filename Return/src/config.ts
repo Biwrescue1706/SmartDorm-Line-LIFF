eed84b7c535d1bf3d5f4fcbbfd5af45d44f85fbc
BiwBong1706 <bewrockgame1@gmail.com>
2025-12-12 11:48:50 +0700

    Update config.ts

--- Return/src/config.ts
+++ Return/src/config.ts
@@ -1,3 +1,2 @@
-export const API_BASE = "https://smartdorm-backend.onrender.com";
-//export const API_BASE = "http://localhost:10000";
+export const API_BASE = import.meta.env.VITE_API_BASE as string;
 export const VITE_LIFF_ID = import.meta.env.VITE_LIFF_ID as string;
