b72d1b466bc7a6facb81d646909e201850519690
BiwBong1706 <bewrockgame1@gmail.com>
2026-01-21 18:36:47 +0700

    Update config.ts

--- Booking/src/config.ts
+++ Booking/src/config.ts
@@ -1,5 +1,5 @@
 //src/config.ts
-export const API_BASE = "https://smartdorm-backend.biwbong.shop";
+export const API_BASE = "https://hub.smartdorm-biwboong.shop";
 //export const API_BASE = "http://localhost:10000";
 
 export const VITE_LIFF_ID = import.meta.env.VITE_LIFF_ID as string;
